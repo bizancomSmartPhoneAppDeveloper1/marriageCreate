54921a3427e04151287064cbcbca7283307e6e3b
yuzuru141 <yuzuru141@hotmail.com>
2014-04-04 14:35:43 +0900

    カメラ機能追加

---
+++ MarriageCreate/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  MarriageCreate
+//
+//  Created by 吉田ひろみち　　　　　　　　　　　　　　　　　　 on 2014/04/02.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
